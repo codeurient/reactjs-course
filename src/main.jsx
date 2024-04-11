eb1b1f239a904dfe9b9222d6246ca8bcd337c034
Mehdi Barasov <asker.20tr@gmail.com>
2024-04-12 00:13:45 +0400

    structure

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+
+// 1) APP.JSX faylini 
+import App from './App.jsx'
+import './index.css'
+
+const el = document.getElementById('root');
+
+ReactDOM.createRoot(el).render( <App /> )
