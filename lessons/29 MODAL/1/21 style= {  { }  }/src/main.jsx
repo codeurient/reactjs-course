4dd879380afe6278ed9b1a88c0d44e70d6489a6d
codeurient <codeurient@gmail.com>
2025-06-19 15:30:08 +0400

    last changes 2

--- lessons/29 MODAL/1/21 style= {  { }  }/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-const el = document.getElementById('root');
-
-ReactDOM.createRoot(el).render( <App /> )
